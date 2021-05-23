f81ec73d50dd9759c94a5c78da722347d988d3fb
gopinath-ganji <gganji100@gmail.com>
2021-05-23 13:31:26 +0530

    Create runnable.java

---
+++ runnable.java
@@ -0,0 +1,16 @@
+public class runnable implements Runnable{
+public  void run()
+{
+for(int i=0;i<10;i++)
+{
+System.out.println(i);
+}
+}
+public static void main(String args[])
+{
+
+runnable d=new runnable();
+Thread t1=new Thread(d);
+t1.start();
+}
+}
